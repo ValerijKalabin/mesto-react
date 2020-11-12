34d8d8e0d9d9482616a49eab5b7426f346308fdb
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-11-12 19:12:56 +0700

    feature: create a function for closing a popup when the overlay is clicked

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -7,8 +7,14 @@ function PopupWithForm({
   onSubmit,
   children
 }) {
+  function closePopup(event) {
+    if(event.target === event.currentTarget) {
+      onClose();
+    }
+  }
+
   return (
-    <div className={`popup popup_task_${name} ${isOpen && 'popup_opened'}`}>
+    <div className={`popup popup_task_${name} ${isOpen && 'popup_opened'}`} onClick={closePopup}>
       <div className="popup__container">
         <h2 className="popup__title">{title}</h2>
         <form className="popup__form" name={name} action="/" method="POST" noValidate onSubmit={onSubmit}>
