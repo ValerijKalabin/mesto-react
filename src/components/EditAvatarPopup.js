cca26f32220d117124cd18a87dfbdec26ac56385
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-11-13 10:41:01 +0700

    feature: create avatar form validation

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -2,37 +2,50 @@ import React from 'react';
 import PopupWithForm from './PopupWithForm';
 
 function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
-  const inputRef = React.useRef();
+  const refInput = React.useRef();
+  const [errorInput, setErrorInput] = React.useState('');
+  const [isValidInput, setValidityInput] = React.useState(false);
+
+  function handleChange() {
+    setErrorInput(refInput.current.validationMessage);
+    setValidityInput(refInput.current.validity.valid);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     onUpdateAvatar({
-      avatar: inputRef.current.value
+      avatar: refInput.current.value
     });
   }
 
   React.useEffect(() => {
-    inputRef.current.value = '';
-  });
+    if(isOpen) {
+      refInput.current.value = '';
+      setErrorInput('');
+      setValidityInput(false);
+    }
+  },  [isOpen]);
 
   return (
     <PopupWithForm
       name="avatar"
       title="Обновить аватар"
       submitButtonCaption="Сохранить"
+      isDisabledSubmitButton={!isValidInput}
       isOpen={isOpen}
       onClose={onClose}
       onSubmit={handleSubmit}
     >
       <input
-        className="popup__input"
+        className={`popup__input ${errorInput && 'popup__input_type_error'}`}
         type="url"
         name="avatarlink"
         placeholder="Ссылка на изображение"
-        ref={inputRef}
+        ref={refInput}
         required
+        onChange={handleChange}
       />
-      <span className="popup__error" />
+      <span className={`popup__error ${errorInput && 'popup__error_visible'}`}>{errorInput}</span>
     </PopupWithForm>
   );
 }
