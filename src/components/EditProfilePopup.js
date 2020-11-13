149e3798df2d88cb459069afe1cd1deceff0de02
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-11-13 09:11:38 +0700

    feature: create profile form validation

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -5,15 +5,25 @@ import CurrentUserContext from '../contexts/CurrentUserContext';
 
 function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
   const currentUser = React.useContext(CurrentUserContext);
-  const [username, setUserName] = useState('');
-  const [description, setDescription] = useState('');
 
-  function handleChangeUserName(event) {
-    setUserName(event.target.value);
+  const [username, setUsername] = useState('');
+  const [errorUsername, setErrorUsername] = useState('');
+  const [isValidUsername, setValidityUsername] = useState(false);
+
+  const [description, setDescription] = useState('');
+  const [errorDescription, setErrorDescription] = useState('');
+  const [isValidDescription, setValidityDescription] = useState(false);
+
+  function handleChangeUsername(event) {
+    setUsername(event.target.value);
+    setErrorUsername(event.target.validationMessage);
+    setValidityUsername(event.target.validity.valid);
   }
 
   function handleChangeDescription(event) {
     setDescription(event.target.value);
+    setErrorDescription(event.target.validationMessage);
+    setValidityDescription(event.target.validity.valid);
   }
 
   function handleSubmit(event) {
@@ -22,8 +32,14 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
   }
 
   React.useEffect(() => {
-    setUserName(currentUser.name);
-    setDescription(currentUser.about);
+    if(isOpen) {
+      setUsername(currentUser.name);
+      setErrorUsername('');
+      setValidityUsername(true);
+      setDescription(currentUser.about);
+      setErrorDescription('');
+      setValidityDescription(true);
+    }
   }, [currentUser, isOpen]);
 
   return (
@@ -31,24 +47,25 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
       name="profile"
       title="Редактировать профиль"
       submitButtonCaption="Сохранить"
+      isDisabledSubmitButton={!isValidUsername || !isValidDescription}
       isOpen={isOpen}
       onClose={onClose}
       onSubmit={handleSubmit}
     >
       <input
-        className="popup__input"
+        className={`popup__input ${errorUsername && 'popup__input_type_error'}`}
         type="text"
         name="username"
         value={username}
-        onChange={handleChangeUserName}
+        onChange={handleChangeUsername}
         placeholder="Имя"
         required
         minLength="2"
         maxLength="40"
       />
-      <span className="popup__error" />
+      <span className={`popup__error ${errorUsername && 'popup__error_visible'}`}>{errorUsername}</span>
       <input
-        className="popup__input"
+        className={`popup__input ${errorDescription && 'popup__input_type_error'}`}
         type="text"
         name="description"
         value={description}
@@ -58,7 +75,7 @@ function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
         minLength="2"
         maxLength="200"
       />
-      <span className="popup__error" />
+      <span className={`popup__error ${errorDescription && 'popup__error_visible'}`}>{errorDescription}</span>
     </PopupWithForm>
   );
 }
